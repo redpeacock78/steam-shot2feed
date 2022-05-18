1d6229524087819348e0d75dff9fdbcf23516206
redpeacock78 <scarletpeacock78@gmail.com>
2022-05-18 19:27:40 +0900

    fix: Fix libs/scrape.js

--- libs/scrape.js
+++ libs/scrape.js
@@ -3,20 +3,23 @@ import * as cheerio from 'cheerio';
 import formatDate from './formatDate.js';
 
 const scrape = async (param) => {
+  const client = ky.create({
+    headers: { 'Accept-Language': 'en,en-US;q=0.9,en;q=0.8' },
+  });
   try {
     const detailUrls = [];
     const desc = [];
     const dates = [];
     const imageUrls = [];
     const url = `https://steamcommunity.com/id/${param}/screenshots/?appid=0&sort=newestfirst&browsefilter=myfiles&view=grid`;
-    const resp = await ky.get(url).text();
+    const resp = await client.get(url).text();
     const $ = cheerio.load(resp);
     $('div.imageWallRow > div > div.floatHelp').map((i, e) =>
       detailUrls.push($(e).find('a').attr('href'))
     );
     for (const url of detailUrls.slice(0, 19)) {
       try {
-        const urlResp = await ky.get(url).text();
+        const urlResp = await client.get(url).text();
         const url$ = cheerio.load(urlResp);
         url$('#caption_ctn > div.screenshotDescription').length !== 0
           ? url$('#caption_ctn > div.screenshotDescription').map((i, e) =>
